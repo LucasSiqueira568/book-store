5b04b4f0f146309baf6dd69f104780e2ecb38c75
LucasSiqueira568 <siqueiralucas-pro@outlook.com>
2021-10-21 18:09:51 -0300

    Criação da base de dados fake para exibição dos books

--- data/books.js
+++ data/books.js
@@ -1,32 +1,91 @@
-const books = [
+export const books = [
     {
         id: 1,
-        name: "Livro 1",
+        name: "Dom Casmurro",
         description: "Primeiro Livro",
         price: 37.90,
-        url: "https://i.pinimg.com/originals/d8/16/c4/d816c439000eea434f997ff7cedebc42.jpg"
+        url: "https://images-na.ssl-images-amazon.com/images/I/613Ywe4F3vL.jpg"
     },
     {
         id: 2,
-        name: "Livro 2",
+        name: "Orgulho e Pre...",
         description: "Primeiro Livro",
         price: 37.90,
         url: "https://images-na.ssl-images-amazon.com/images/I/51kS4++58fL._SY344_BO1,204,203,200_.jpg"
     },
     {
         id: 3,
-        name: "Livro 3",
+        name: "E Não Sobrou...",
         description: "Primeiro Livro",
         price: 37.90,
-        url: "https://images-na.ssl-images-amazon.com/images/I/51ASlhVRLRL._SX345_BO1,204,203,200_.jpg"
+        url: "https://images-na.ssl-images-amazon.com/images/I/51QVh6K9cVL._SX342_SY445_QL70_ML2_.jpg"
     },
     {
         id: 4,
-        name: "Livro 4",
+        name: "Prisioneiros Da..",
         description: "Primeiro Livro",
         price: 37.90,
-        url: "https://images-na.ssl-images-amazon.com/images/I/41Io2OPwS-L._SY344_BO1,204,203,200_QL70_ML2_.jpg"
+        url: "https://m.media-amazon.com/images/I/51wwQqK0dtL.jpg"
+    },
+    {
+        id: 5,
+        name: "O Morro dos...",
+        description: "Primeiro Livro",
+        price: 37.90,
+        url: "https://images-na.ssl-images-amazon.com/images/I/71fcsbZKaGL.jpg"
+    },
+    {
+        id: 6,
+        name: "A Arvore dos...",
+        description: "Primeiro Livro",
+        price: 37.90,
+        url: "http://www.intrinseca.com.br/upload/livros/ArvoreDosDesejo-G.jpg"
     },
 ];
 
-export default books
+export const books_recomend = [
+    {
+        id: 1,
+        name: "O Físico",
+        description: "Primeiro Livro",
+        price: 37.90,
+        url: "https://images-na.ssl-images-amazon.com/images/I/81ypPVmWXAL.jpg"
+    },
+    {
+        id: 2,
+        name: "Harry Potter...",
+        description: "Primeiro Livro",
+        price: 37.90,
+        url: "https://d3ugyf2ht6aenh.cloudfront.net/stores/419/354/products/81ntln-kz7l-49ef0c78b678796d6415894921326696-1024-1024.jpg"
+    },
+    {
+        id: 3,
+        name: "Memórias Póstumas...",
+        description: "Primeiro Livro",
+        price: 37.90,
+        url: "https://m.media-amazon.com/images/I/51GnxyfIDML.jpg"
+    },
+    {
+        id: 4,
+        name: "Os Quatro A...",
+        description: "Primeiro Livro",
+        price: 37.90,
+        url: "https://m.media-amazon.com/images/I/51yghA6UWpL.jpg"
+    },
+    {
+        id: 5,
+        name: "1984",
+        description: "Primeiro Livro",
+        price: 37.90,
+        url: "https://images-na.ssl-images-amazon.com/images/I/81EStZoMf7L.jpg"
+    },
+    {
+        id: 6,
+        name: "A Revolução...",
+        description: "Primeiro Livro",
+        price: 37.90,
+        url: "https://images-na.ssl-images-amazon.com/images/I/618iHJVMh4L._SX342_SY445_QL70_ML2_.jpg"
+    },
+];
+
+const appBooks = {books_recomend, books}

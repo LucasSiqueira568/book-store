689b649e668ebf3a1e292edf623e8c2ef4975e7d
Lucas Siqueira <siqueiralucas-pro@outlook.com>
2021-10-23 19:39:56 -0300

    Inserindo novos livros a base de dados

---
+++ data/books.ts
@@ -0,0 +1,92 @@
+export const books = [
+  {
+    id: 1,
+    name: "Dom Casmurro",
+    description:
+      "Em Dom Casmurro, o narrador Bento Santiago retoma a infância que passou na Rua de Matacavalos e conta a história do amor e das desventuras que viveu com Capitu, uma das personagens mais enigmáticas e intrigantes da literatura brasileira. Nas páginas deste romance, encontra-se a versão de um homem perturbado pelo ciúme, que revela aos poucos sua psicologia complexa e enreda o leitor em sua narrativa ambígua acerca do acontecimento ou não do adultério da mulher com olhos de ressaca, uma das maiores polêmicas da literatura brasileira.",
+    price: 11.50,
+    url: "https://images-na.ssl-images-amazon.com/images/I/613Ywe4F3vL.jpg",
+  },
+  {
+    id: 2,
+    name: "Orgulho e Preco...",
+    description: "Orgulho e preconceito é o livro mais famoso de Jane Austen e possui uma série de personagens inesquecíveis e um enredo memorável. Austen nos apresenta Elizabeth Bennet como heroína irresistível e seu pretendente aristocrático, o sr. Darcy. Nesse livro, aspectos diferentes são abordados: orgulho encontra preconceito, ascendência social confronta desprezo social, equívocos e julgamentos antecipados conduzem alguns personagens ao sofrimento e ao escândalo. O livro pode ser considerado a obra-prima da escritora, que equilibra comédia com seriedade, observação meticulosa das atitudes humanas e sua ironia refinada. A nova coleção possui capa dura e estilo inspirado nos bullet journals.",
+    price: 38.67,
+    url: "https://images-na.ssl-images-amazon.com/images/I/51kS4++58fL._SY344_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 3,
+    name: "E Não Sobrou...",
+    description: "Uma ilha misteriosa, um poema infantil, dez soldadinhos de porcelana e muito suspense são os ingredientes com que Agatha Christie constrói seu romance mais importante. Na ilha do Soldado, antiga propriedade de um milionário norte-americano, dez pessoas sem nenhuma ligação aparente são confrontadas por uma voz misteriosa com fatos marcantes de seus passados.",
+    price: 39.9,
+    url: "https://images-na.ssl-images-amazon.com/images/I/51QVh6K9cVL._SX342_SY445_QL70_ML2_.jpg",
+  },
+  {
+    id: 4,
+    name: "Prisioneiros Da..",
+    description: "Um magnata poderoso. Um império tecnológico. E uma família dilacerada. Theo Fester conseguiu vencer uma infância de pobreza e bullying para se tornar um empreendedor mundialmente conhecido. Sua vida pessoal, entretanto, não seguiu o mesmo caminho: ele e seus filhos vivem uma farsa, se digladiando por poder e atenção. Ao se dar conta de que sua família está aprisionada por cárceres mentais, Theo precisará se reinventar mais uma vez e mudar radicalmente seus relacionamentos, antes que seja tarde demais.",
+    price: 17.9,
+    url: "https://m.media-amazon.com/images/I/51wwQqK0dtL.jpg",
+  },
+  {
+    id: 5,
+    name: "O Morro dos...",
+    description: "Único romance da escritora inglesa Emily Bronte, O morro dos ventos uivantes retrata uma trágica historia de amor e obsessão em que os personagens principais são a obstinada e geniosa Catherine Earnshaw e seu irmão adotivo, Heathcliff. Grosseiro, humilhado e rejeitado, ele guarda apenas rancor no coração, mas tem com Catherine um relaciona- mento marcado por amor e, ao mesmo tempo, ódio. Essa ligação perdura mesmo com o casamento de Catherine com Edgar Linton.",
+    price: 8.9,
+    url: "https://images-na.ssl-images-amazon.com/images/I/71fcsbZKaGL.jpg",
+  },
+  {
+    id: 6,
+    name: "Arvore dos Desejos",
+    description: "Árvores não sabem contar piadas, mas contam ótimas histórias. E Red é um carvalho centenário que já viu de tudo em seus muitos anos de vida. Também é a árvore dos desejos do bairro, e todo ano, no dia 1º de maio, as pessoas amarram em seus galhos fitas ou tiras de tecido com os mais diversos pedidos, sonhos e anseios.",
+    price: 24.9,
+    url: "http://www.intrinseca.com.br/upload/livros/ArvoreDosDesejo-G.jpg",
+  },
+];
+
+export const books_recomend = [
+  {
+    id: 1,
+    name: "O Físico",
+    description: "Mais que uma recriação histórica magistral, aqui está também a história fantástica de uma vocação para a Medicina. O romance de Noah Gordon recria o século XI de maneira tão eloquente que o leitor é levado em suas centenas de páginas por uma onda gigantesca de autenticidade e imaginação. O Publishers Weekly assim se referiu a O físico, de Noah Gordon, autor festejado como um dos maiores nomes do mundo literário norte-americano e presente em todas as listas dos livros mais vendidos",
+    price: 27.93,
+    url: "https://images-na.ssl-images-amazon.com/images/I/81ypPVmWXAL.jpg",
+  },
+  {
+    id: 2,
+    name: "Harry Potter",
+    description: "Primeiro Livro",
+    price: 37.9,
+    url: "https://d3ugyf2ht6aenh.cloudfront.net/stores/419/354/products/81ntln-kz7l-49ef0c78b678796d6415894921326696-1024-1024.jpg",
+  },
+  {
+    id: 3,
+    name: "Memórias Póstumas de Brás Cubas",
+    description: "Brás Cubas está morto. Mas isso não o impede de relatar em seu livro os acontecimentos de sua existência e de sua grande ideia fixa: lançar o Emplasto Brás Cubas. Deus te livre, leitor, de uma ideia fixa. O medicamento anti-hipocondríaco torna-se o estopim de uma série de lembranças, reminiscências e digressões da vida do defunto autor.",
+    price: 14.90,
+    url: "https://m.media-amazon.com/images/I/51GnxyfIDML.jpg",
+  },
+  {
+    id: 4,
+    name: "Os Quatro Amores",
+    description: "Em Os quatro amores, um de seus livros mais influentes, agora enriquecido por nova tradução, Lewis contempla a essência do amor e avalia como cada tipo se ajusta aos demais. Com a maestria que o tornou um dos autores mais importantes do século XX, ele desafia e incorpora definições clássicas do amor de uma forma que continua atual e relevante. Como lembra o autor, foi por amor que Deus fez existir criaturas inteiramente supérfluas, somente a fim de poder amá-las e aperfeiçoá-las",
+    price: 22.41,
+    url: "https://m.media-amazon.com/images/I/51yghA6UWpL.jpg",
+  },
+  {
+    id: 5,
+    name: "1984",
+    description: "Winston Smith, número 6079, ocupa uma função comum no Departamento de Documentação: “retificar” fatos de acordo com as orientações do Ministério da Verdade. E ele não mede esforços em seu objetivo de erradicar, em todos os níveis, quaisquer elementos que ameacem sua existência. A individualidade, o livre-arbítrio, o pensamento crítico, o amor – todos considerados transgressões inadmissíveis – são punidos com o máximo de rigor e severidade.",
+    price: 9.9,
+    url: "https://images-na.ssl-images-amazon.com/images/I/81EStZoMf7L.jpg",
+  },
+  {
+    id: 6,
+    name: "A Revolução dos Bichos",
+    description: "Verdadeiro clássico moderno, concebido por um dos mais influentes escritores do século XX, A revolução dos bichos é uma fábula sobre o poder. Narra a insurreição dos animais de uma granja contra seus donos. Progressivamente, porém, a revolução degenera numa tirania ainda mais opressiva que a dos humanos.",
+    price: 13.52,
+    url: "https://images-na.ssl-images-amazon.com/images/I/618iHJVMh4L._SX342_SY445_QL70_ML2_.jpg",
+  },
+];
+
+const appBooks = { books_recomend, books };
